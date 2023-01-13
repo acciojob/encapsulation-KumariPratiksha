bfa1e02b0e043a35f21234e5771c0473bef761b1
Pratiksha Kumari <pratikshaakumari@gmail.com>
2023-01-13 20:45:45 +0530

    getter setter

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,17 @@
 package com.driver;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        RWOnly obj = new RWOnly();
+//        java: name has private access in com.driver.RWOnly
+        obj.setName(sc.nextLine());
+        System.out.println(obj.getName());
+
+    }
+
   
 }
